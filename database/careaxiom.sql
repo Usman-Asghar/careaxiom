4fcc2a0e2d0cce6c938b47a6623147234a0dc25f
Usman Asghar <usmanasghar.pk@gmail.com>
2016-03-09 04:22:25 +0500

    Upload Database

---
+++ database/careaxiom.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2016 at 12:21 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `careaxiom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airport`
+--
+
+CREATE TABLE IF NOT EXISTS `airport` (
+`id` int(9) NOT NULL,
+  `airport_code` varchar(255) NOT NULL,
+  `airport_name` char(255) NOT NULL,
+  `country` char(255) NOT NULL,
+  `city` char(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `airport`
+--
+
+INSERT INTO `airport` (`id`, `airport_code`, `airport_name`, `country`, `city`) VALUES
+(11, 'AL123', 'Allama Iqbal International Airport', 'Pakistan', 'Lahore');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `airport`
+--
+ALTER TABLE `airport`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `airport`
+--
+ALTER TABLE `airport`
+MODIFY `id` int(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
